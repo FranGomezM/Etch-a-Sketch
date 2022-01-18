44e2246d1f467bdb8ba93af47f4ab74483298b55
FranGomezM <travecedo991@gmail.com>
2022-01-18 20:47:51 +0000

    codingthe reset button  and the dynamic size grid

--- index.js
+++ index.js
@@ -1,44 +1,69 @@
-function getRow(num) {
-    const container = document.querySelector('#container');
+const container = document.querySelector('#container');
+for (let i = 0; i < 16; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
     container.appendChild(row);
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i < 16; i++) {
         const cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.setAttribute('style', 'width: 40px; height: 40px;');
         row.appendChild(cell);
     }
 }
 
-function getGrid(num) {
-    for (let i = 0; i < num; i++) {
-        getRow(num);
-    }
-}
-
-getGrid(16);
-
 const cells = document.querySelectorAll('.cell');
 const cell = document.querySelector('.cell');
-
-cells.forEach((cell) => {
+cells.forEach( (cell) =>  {
     cell.addEventListener('mouseover', () => {
-        cell.setAttribute('style', 'background-color: grey;')
+        cell.setAttribute('style', 'background-color: grey; width: 40px; height: 40px;');
     })
 })
 
-function resetDraw() {
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', () => {
+    cells.forEach((cell) => {
+        cell.setAttribute('style', 'background-color: white; width: 40px; height: 40px;');
+    })
+})
+
+const size = document.querySelector('#size');
+size.addEventListener('click', () =>{
+    let row = container.firstElementChild;
+    while (row) {
+        container.removeChild(row);
+        row = container.firstElementChild;
+    }
+})
+
+size.addEventListener('click', () => {
+    let gridSize = prompt('Write the new grid size...');
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for (let i = 0; i < gridSize; i++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+    }
+
     const cells = document.querySelectorAll('.cell');
-    const cell = document.querySelector('.cell')
-    cells.forEach(() => {
-        cell.setAttribute('style', 'backgroundcolor: white;')
-    });
-    const button = document.querySelector('#reset');
-    button.addEventListener('click', () => {
-        let num = prompt("write the box size");
-        console.log(num);
-        return num;
-    });
-}
+    const cell = document.querySelector('.cell');
+    gridSize = 640/gridSize;
+    cells.forEach((cell) => {
+        cell.setAttribute('style', `width: ${Number(gridSize)}px; height: ${Number(gridSize)}px;`);
+    })
 
-
+    cells.forEach( (cell) =>  {
+        cell.addEventListener('mouseover', () => {
+            cell.setAttribute('style', `background-color: grey; width: ${Number(gridSize)}px; height: ${Number(gridSize)}px;`);
+        })
+    })
+    const reset = document.querySelector('#reset');
+    reset.addEventListener('click', () => {
+        cells.forEach((cell) => {
+            cell.setAttribute('style', `background-color: white; width: ${Number(gridSize)}px; height: ${Number(gridSize)}px;`);
+        })
+    })
+})
--- test.js
+++
@@ -1,9 +0,0 @@
-function getSize(num) {
-    let size = 640/num;
-    const cells = document.querySelectorAll('.cell');
-    const cell = document.querySelector('.cell');
-    cells.forEach(() => {
-        cell.setAttribute('style', `width: ${size}; height: ${size}`)
-    });
-    return size;
-}
